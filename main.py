901c343b4000e36a47c04ec2fab51886473c3629
FU <1149124450@qq.com>
2025-09-26 00:51:26 +0800

    update run

--- main.py
+++ main.py
@@ -74,13 +74,7 @@ async def init_db():
 
 
 if __name__ == "__main__":
-    asyncio.run(init_db())
+    # asyncio.run(init_db())
     import uvicorn
 
-    uvicorn.run(
-        "main:app",
-        host="127.0.0.1",
-        port=8002,
-        # reload=True,
-        log_level="info"
-    )
+    uvicorn.run(app, host="127.0.0.1", port=8002, log_level="info")
